ff64f261a49a8606c184abfd2b43b81400b2e805
Matthew Giannini <matthewg.dev@gmail.com>
2014-01-03 20:48:08 -0500

    Haddock doc.

--- Cards.hs
+++ Cards.hs
@@ -4,15 +4,6 @@ import Data.Char (chr)
 import qualified Data.Text as T
 import System.Random
 
--- main = do
---   let tenDecks = take 10 randomDecks
---       prettyDecks = map (\d -> foldl1 (++) (map showCard d)) tenDecks
---     in mapM_ putStrLn  prettyDecks
---   return ()
-
--- randomDecks :: [[Card]]
--- randomDecks = map (\seed -> shuffle' (mkStdGen seed)) [1..]
-
 data Suit = Club
           | Diamond
           | Heart
@@ -37,28 +28,38 @@ data Rank = Ace
 data Card = Card Rank Suit
             deriving (Eq, Show)
 
+-- |Get the suit of a 'Card'
 getSuit :: Card -> Suit
 getSuit (Card _ s) = s
 
+-- |Get the rank of a 'Card'
 getRank :: Card -> Rank
 getRank (Card r _) = r
 
 -- Printing Utilities
+--
+
+-- |Get the unicode offset for the beginning (Ace) of each suit.
 unicodeSuitOffset :: Suit -> Int
 unicodeSuitOffset Club = 127185
 unicodeSuitOffset Diamond = 127169
 unicodeSuitOffset Heart = 127153
 unicodeSuitOffset Spade = 127137
 
+-- |Get the unicode offset of a rank. Add this to the suit unicode offset to obtain
+-- the proper card.
 unicodeRankOffset :: Rank -> Int
 unicodeRankOffset rank
-  | rank == Queen || rank == King = fromEnum rank + 1
+  | rank == Queen || rank == King = fromEnum rank + 1 -- Skip the weird 'Knight' unicode card
   | otherwise = fromEnum rank
 
+-- |Get the unicode string for a playing card.
 showCard :: Card -> String
 showCard (Card rank suit) =
+  -- Append blank so that cards show properly on a terminal. They tend to run together without this.
   (++" ") . T.unpack . T.singleton . chr $ (unicodeSuitOffset suit) + (unicodeRankOffset rank)
 
+-- |Get the unicode string for a list of playing cards.
 showCards :: [Card] -> String
 showCards [] = ((++" ") . T.unpack . T.singleton) '\127136'
 showCards cards = concatMap showCard cards
--- IdiotsDelight.hs
+++ IdiotsDelight.hs
@@ -3,28 +3,41 @@ module IdiotsDelight where
 import Cards
 import System.Random
 
+{-|
+  A game of Idiot's Delight is modeled as the current state of
+  the pile (undrawn cards) and the cards in the player's hand.
+-}
 data Game = Game
             { pile :: [Card]
             , hand :: [Card]
             }
 
+-- |Create a new game from a list of 'Card's
 newGame :: [Card] -> Game
 newGame cards = Game cards []
 
+-- |Given a game state, return a new game state where a card has been
+-- drawn from the pile and placed in the player's hand.
 pullFromPile :: Game -> Game
 pullFromPile (Game p h) = Game (drop 1 p) ((head p):h)
 
+-- |Given a game state, return a new game state where the player's
+-- hand has been reduced once. If the player's hand cannot be reduced,
+-- then a card is pulled from the pile.
 reduceHand :: Game -> Game
 reduceHand game@(Game p (c1:_:_:c4:cs))
   | getRank c1 == getRank c4 = Game p cs
   | getSuit c1 == getSuit c4 = Game p (c1:c4:cs)
   | otherwise = pullFromPile game
 
+-- |Play one turn of the game. Either the hand will be reduced or a
+-- card will be pulled from the pile.
 doTurn :: Game -> Game
 doTurn game@(Game _ h)
   | length h < 4 = pullFromPile game
   | otherwise = reduceHand game
 
+-- |Play a game of Idiot's Delight until a win/loss state is achieved.
 playGame :: Game -> IO ()
 playGame (Game [ ] [ ]) = putStrLn "You Win!!!"
 playGame (Game [ ]  _ ) = putStrLn "You Lose :-("
